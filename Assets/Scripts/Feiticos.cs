215b2c8d7582556f7e0b1e5d2544c3c96ca3fea1
castromir <caiocastromir@gmail.com>
2024-05-15 13:51:59 -0300

    troca versao unity para 2018 e teleporte

---
+++ Assets/Scripts/Feiticos.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Feiticos : ScriptableObject
+{
+    public string nome;
+    public float tempoRecarga;
+    public float tempoDuracao;
+
+    public virtual void Ativar(GameObject parente) {}
+    public virtual void RecargaComecar(GameObject parente) {}
+}
+
---
+++ Assets/Scripts/GuardaFeitico.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GuardaFeitico : MonoBehaviour
+{
+    public Feiticos feitico;
+    float tempoRecarga;
+    float tempoDuracao;
+
+    enum FeiticoEstado
+    {
+        pronto,
+        ativo,
+        recarga
+    }
+    FeiticoEstado estado = FeiticoEstado.ativo;
+
+    public KeyCode atalho;
+    
+    void Update()
+    {
+        switch (estado)
+        {
+            case FeiticoEstado.pronto:
+                if (Input.GetKeyDown(atalho))
+                {
+                    feitico.Ativar(gameObject);
+                    estado = FeiticoEstado.ativo;
+                    tempoDuracao = feitico.tempoDuracao;
+                }
+                break;
+            case FeiticoEstado.ativo:
+                if (tempoDuracao > 0)
+                {
+                    tempoDuracao -= Time.deltaTime;
+                }
+                else 
+                {
+                    feitico.RecargaComecar(gameObject);
+                    estado = FeiticoEstado.recarga;
+                    tempoRecarga = feitico.tempoRecarga;
+                }
+                break;
+            case FeiticoEstado.recarga:
+                if (tempoRecarga > 0) 
+                {
+                    tempoRecarga -= Time.deltaTime;
+                }
+                else
+                {
+                    estado = FeiticoEstado.pronto;
+                }
+                break;        
+        }
+    }
+}
