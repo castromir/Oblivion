192afc5acd91486e745ddcb47212084a2288477b
Moises <moisesprotazio@gmail.com>
2024-05-18 11:45:48 -0300

    Adicionei o boss e o pder de cair estrela, o dash ainda nao esta funcioandno do jeito que deve funcionar.

---
+++ Assets/Characters/HappyBoss/AIFollow.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Threading;
+using UnityEngine;
+
+public class AIFollow : MonoBehaviour
+{
+    public GameObject player;
+    public float speed;
+    public float acceptableDistance;
+    private Powers powers;
+
+    private float distance;
+    public float timerDistance;
+    private float timer = 0f;
+    public float dashDistance;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        powers = GetComponent<Powers>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        distance = Vector2.Distance(transform.position, player.transform.position);
+        Vector2 direction = player.transform.position - transform.position;
+        direction.Normalize();
+        if (distance < acceptableDistance)
+        {
+            transform.position = Vector2.MoveTowards(this.transform.position, player.transform.position, speed * Time.deltaTime);
+
+            timer += Time.deltaTime;
+            if (timer > timerDistance)
+            {
+                powers.SpawnStar();
+                timer = 0f;
+            }
+        }
+        if (distance < dashDistance) 
+        {
+            
+            powers.DashAtacck();
+            
+        }
+    }
+
+
+}
---
+++ Assets/Characters/HappyBoss/Powers.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Powers : MonoBehaviour
+{
+    //Relacionados ao ataque de estrela
+    public GameObject player;
+    public GameObject starPower;
+    public float compensarCamera;
+    public float lowestPoit, highestPoit;
+
+
+    public void SpawnStar()
+    {
+
+
+        Vector3 spawnPos = player.transform.position;
+        spawnPos.y = player.transform.position.y + Random.Range(lowestPoit, highestPoit) + compensarCamera;
+        spawnPos.x = player.transform.position.x + Random.Range(lowestPoit, highestPoit);
+
+        Instantiate(starPower, spawnPos, Quaternion.identity);
+
+
+
+
+    }
+    
+    //Relacionados ao ataque de chegar perto
+   
+    public float dashSpeed;
+    public void DashAtacck()
+    {
+        
+
+        transform.position = Vector2.MoveTowards(this.transform.position, player.transform.position, dashSpeed * Time.deltaTime);
+
+    }
+    
+}
