dc6258fa1046f8da738e72fbb14230ccf00f2712
Moises <moisesprotazio@gmail.com>
2024-06-08 19:20:30 -0300

    Eu tentei fazer a logica do dano da estrela, mas não estou conseguindo verificar se a estrela esta colidindo com o player

--- Assets/Scripts/Boss/StarScript.cs
+++ Assets/Scripts/Boss/StarScript.cs
@@ -9,25 +9,51 @@ public class StarScript : MonoBehaviour
     public Powers powers;
     private GameObject markStar;
     public GameObject mark;
+    public PlayerVida playerVida;
+    private bool isOnGoud;
 
     void Start()
     {
+        isOnGoud = false;
+
         powers = FindObjectOfType<Powers>();
         inicio = transform.position.y;
 
         Vector3 spawnPos = transform.position;
         spawnPos.y = spawnPos.y - powers.compensarCamera;
         markStar = Instantiate(mark,spawnPos, Quaternion.identity);
+
+        playerVida = FindObjectOfType<PlayerVida>();
     }
 
     // Update is called once per frame
     void Update()
     {
+    
         transform.position += Vector3.down * moveSpeed * Time.deltaTime;
         if (transform.position.y < inicio - powers.compensarCamera)
         {
+            
             Destroy(gameObject);
             Destroy(markStar);
+        }
+    }
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Mark"))
+        {
+            isOnGoud = true;
+            Debug.Log("ta no chao");
+        }
+        
+
+    }
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            playerVida.ReceberDano();
+            Debug.Log("ta escostando");
         }
     }
 }
