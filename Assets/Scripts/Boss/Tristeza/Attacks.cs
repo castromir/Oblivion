b518782ad9b5eb8c1afea53ef9abdfe78725d29b
Moises <moisesprotazio@gmail.com>
2024-06-30 18:13:11 -0300

    Fiz a arena do boss da tristeza, o prototipo inicial do boss, e um ataque para todas as direções, falta fazer a vida do boss, e o ataque deixar o player lento

---
+++ Assets/Scripts/Boss/Tristeza/Attacks.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Security.Cryptography.X509Certificates;
+using UnityEngine;
+
+public class Attacks : MonoBehaviour
+{
+    public GameObject projectilePrefab;
+    public float projectileSpeed = 5f;
+    public int numberOfProjectiles = 8;
+    public float spreadAngle = 360f;
+
+    private float canAttack=0f;
+    public float AttackTime =5f;
+
+    void Start()
+    {
+        
+    }
+    void Update()
+    {
+        canAttack += Time.deltaTime;
+        if(canAttack > AttackTime) 
+            LaunchProjectilesInAllDirections();
+            
+    }
+
+    void LaunchProjectilesInAllDirections()
+    {
+        float angleStep = spreadAngle / numberOfProjectiles;
+        float angle = 0f;
+
+        for (int i = 0; i < numberOfProjectiles; i++)
+        {
+            float projectileDirX = transform.position.x + Mathf.Sin((angle * Mathf.PI) / 180f);
+            float projectileDirY = transform.position.y + Mathf.Cos((angle * Mathf.PI) / 180f);
+
+            Vector3 projectileMoveVector = new Vector3(projectileDirX, projectileDirY, 0f);
+            Vector2 projectileDir = (projectileMoveVector - transform.position).normalized;
+
+            GameObject proj = Instantiate(projectilePrefab, transform.position, Quaternion.identity);
+            proj.GetComponent<Rigidbody2D>().velocity = new Vector2(projectileDir.x * projectileSpeed, projectileDir.y * projectileSpeed);
+
+            angle += angleStep;
+        }
+        canAttack = 0f;
+    }
+}
+
---
+++ Assets/Scripts/Boss/Tristeza/BallScript.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallScript : MonoBehaviour
+{
+
+    public PlayerVida playerVida;
+    void Start()
+    {
+        playerVida = FindObjectOfType<PlayerVida>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.gameObject.CompareTag("Player")) 
+        {
+            playerVida.ReceberDano();
+            Debug.Log("Bola de neve da dano");
+            Destroy(gameObject);
+            
+        }
+        if (other.gameObject.CompareTag("Parede"))
+        {
+            Debug.Log("Bateu na parede");
+            Destroy(gameObject);
+        }
+       
+    }
+}
