6cfee91a68b0451f0c7e8a7f3c73ec0219463495
Moises <moisesprotazio@gmail.com>
2024-07-15 18:20:08 -0300

    Tentei arrumar o teleport, mas nao funcionou

---
+++ Assets/Scripts/Player/TesteDoTeleport.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TesteDoTeleport : MonoBehaviour
+{
+    public bool teste = false;
+    // Start is called before the first frame update
+
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.CompareTag("Chao"))
+        {
+            teste = true;
+            Debug.Log("Esta no chao");
+        }
+        Debug.Log("Colisao detectada");
+        
+    }
+}
--- Assets/Scripts/TeleporteFeitico.cs
+++ Assets/Scripts/TeleporteFeitico.cs
@@ -5,13 +5,24 @@ using UnityEngine;
 [CreateAssetMenu]
 public class TeleporteFeitico : Feiticos
 {
+    public GameObject testePrefab;
     public override void Ativar(GameObject parente)
     {
         Vector3 mousePosicao = Camera.main.ScreenToWorldPoint(Input.mousePosition);
         mousePosicao.z = 0;
 
-        //if (mousePosicao.y.T)
+        GameObject testeInstance = Instantiate(testePrefab, mousePosicao, Quaternion.identity);
 
-        parente.transform.position = mousePosicao;
+       
+
+        TesteDoTeleport testeScript = testeInstance.GetComponent<TesteDoTeleport>();
+
+        if (testeScript.teste)
+        {
+            parente.transform.position = mousePosicao;
+        }
+
+        Destroy(testeInstance);
+
     }
 }
